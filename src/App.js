ed57fea07865360adf4c2428c870ca1da10221f7
Emily (Thi) Nguyen <emily.tn.98@gmail.com>
2022-10-04 10:21:52 -0500

    react flow code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from "react";
+
+import OverviewFlow from "./Flow";
+
+function App() {
+  return (
+    <div className="App">
+      <OverviewFlow />
+    </div>
+  );
+}
+
+export default App;
